f6c89b708afecb792c5e275a8254aae009d247c3
Julius Ossai <ossaije1@gmail.com>
2023-05-21 02:23:00 +0100

    Add project files.

---
+++ CatalogService/CatalogService/Controllers/ItemsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Test 1", "Test 1 description", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Test 2", "Test 2 description", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Test 3", "Test 3 description", 9, DateTimeOffset.UtcNow)
+        };
+
+        // GET: api/<ItemsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ItemsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ItemsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ItemsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
