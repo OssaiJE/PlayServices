7a653c7e772d3dbc8daa019b112fd9b3d511c9c0
Julius Ossai <ossaije1@gmail.com>
2023-06-30 14:15:56 +0100

    completed implementation of respository

--- CatalogService/CatalogService/Repositories/ItemsRepository.cs
+++ CatalogService/CatalogService/Repositories/ItemsRepository.cs
@@ -16,15 +16,47 @@ namespace CatalogService.Repositories
             dbCollection = database.GetCollection<ItemEntity>(collectionName);
         }
 
+        //Get all items respository
         public async Task<IReadOnlyCollection<ItemEntity>> GetAllItems()
         {
             return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
         }
 
+        //Get one Item repository
         public async Task<ItemEntity> GetOneItem(Guid id)
         {
             FilterDefinition<ItemEntity> filter = filterBuilder.Eq(entity => entity.Id, id);
             return await dbCollection.Find(filter).SingleOrDefaultAsync();
+        }
+
+        //Create Item repository
+        public async Task CreateItem(ItemEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+        //Update Item repository
+        public async Task UpdateItem(ItemEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            FilterDefinition<ItemEntity> filter = filterBuilder.Eq(existingEntity=> existingEntity.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        //Delete Item repository
+        public async Task RemoveItem(Guid id)
+        {
+            FilterDefinition<ItemEntity> filter = filterBuilder.Eq(entity => entity.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
         }
     }
 }
