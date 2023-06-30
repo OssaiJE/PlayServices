08e59dc787c96fd90dc92a0fbea831268795713c
Julius Ossai <ossaije1@gmail.com>
2023-06-30 04:30:31 +0100

    started working on entities and repositories

--- CatalogService/CatalogService/Dtos.cs
+++ CatalogService/CatalogService/Dtos.cs
@@ -1,6 +1,9 @@
-﻿namespace CatalogService
+﻿using System.ComponentModel.DataAnnotations;
+
+//Add "?" to data types to make fields nullable
+namespace CatalogService
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
-    public record CreateItemDto(string Name, string Description, decimal Price);
-    public record UpdateItemDto(string Name, string Description, decimal Price);
+    public record CreateItemDto(string Name, string Description, [Range(0, 1000)] decimal Price);
+    public record UpdateItemDto(string Name, string Description, [Range(0, 1000)] decimal Price);
 }
