cd10859b8fa5d86329e56b9644ce6c0ed5f6e946
Julius Ossai <ossaije1@gmail.com>
2023-06-30 05:18:11 +0100

    started integrating mongodb with repository services

---
+++ CatalogService/CatalogService/Repositories/ItemsRepository.cs
@@ -0,0 +1,24 @@
+﻿using CatalogService.Entities;
+using MongoDB.Driver;
+
+namespace CatalogService.Repositories
+{
+    public class ItemsRepository
+    {
+        private const string collectionName = "items";
+        private readonly IMongoCollection<ItemEntity> dbCollection;
+        private readonly FilterDefinitionBuilder<ItemEntity> filterBuilder = Builders<ItemEntity>.Filter;
+
+        public ItemsRepository()
+        {
+            var mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+            var database = mongoClient.GetDatabase("Catalog");
+            dbCollection = database.GetCollection<ItemEntity>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<ItemEntity>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+    }
+}
